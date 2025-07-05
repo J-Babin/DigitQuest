fd7ab4bd9588db0d0f2cdd2046911acf188670eb
J-Babin <babin.jeremy29@gmail.com>
2025-07-05 23:46:11 +0200

    First commit

---
+++ src/main/java/com/skazy/DigitQuest/DigitQuestApplication.java
@@ -0,0 +1,13 @@
+package com.skazy.DigitQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skazy/DigitQuest/DigitQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skazy.DigitQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
