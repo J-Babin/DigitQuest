e88d063c83bd1e878606ff6817c372bf3038d0c6
J-Babin <babin.jeremy29@gmail.com>
2025-07-17 16:13:13 +0200

    feat(init): init frontend project

---
+++ digitquest-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline text-center mt-10 text-blue-500">
+        Simple React Typescript Tailwind Sample
+      </h2>
+      <p className="text-center mt-4 text-blue-600">This is a simple example of a React app using TypeScript and Tailwind CSS.</p>
+
+    </>
+  )
+}
+
+export default App
---
+++ digitquest-frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       primary: {
+  //         50: '#f0f9ff',
+  //         500: '#3b82f6',
+  //         600: '#2563eb',
+  //       },
+  //       purple: {
+  //         50: '#faf5ff',
+  //         100: '#f3e8ff',
+  //         500: '#8b5cf6',
+  //       }
+  //     }
+  //   },
+  // },
+  plugins: [],
+}
